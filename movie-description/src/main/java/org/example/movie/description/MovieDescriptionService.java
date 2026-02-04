ec0d330bf30083c31d8989f38f4c9de3e358a8be
gtmcc <109885476+glenn-mccrea@users.noreply.github.com>
2026-02-03 22:35:32 -0600

    working app

---
+++ movie-description/src/main/java/org/example/movie/description/MovieDescriptionService.java
@@ -0,0 +1,26 @@
+package org.example.movie.description;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import org.apache.http.HttpException;
+
+import java.io.IOException;
+
+public class MovieDescriptionService {
+    Client client = new Client();
+
+    public MovieDescriptionService(){
+        this.client = new Client();
+    }
+
+    public String generateDescription(String movieTitle) throws HttpException, IOException {
+        String prompt = "generate a one sentence description of the movie " +movieTitle;
+        GenerateContentResponse response = client.models.generateContent("gemini-2.0-flash-001", prompt, null);
+        return response.text();
+    }
+//
+//    System.out.println("Enter a question for the model:");
+//    String query = scanner.nextLine();
+//    GenerateContentResponse response = client.models.generateContent("gemini-2.0-flash-001", query, null);
+//        System.out.println(response.text());
+}
--- movie-description/src/main/java/org/example/movie/description/controller/MovieController.java
+++ movie-description/src/main/java/org/example/movie/description/controller/MovieController.java
@@ -1,31 +1,34 @@
 package org.example.movie.description.controller;
 
-
+import org.apache.http.HttpException;
+import org.example.movie.description.MovieDescriptionService;
 import org.example.movie.description.model.Movie;
 import org.example.movie.description.repositories.MovieRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
     private final MovieRepository movieRepository;
+    private MovieDescriptionService descriptionService;
+
     public MovieController(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
+        this.descriptionService = new MovieDescriptionService();
     }
     @GetMapping
     public List<Movie> getAllItems() {
         return movieRepository.findAll();
     }
-    @GetMapping("/{id}")
-    public Optional<Movie> getItem(@PathVariable int id) {
-        return movieRepository.findById(id);
-    }
+
     @PostMapping
-    public Movie addItem(@RequestBody Movie movie) {
+    public Movie addItem(@RequestBody Movie movie) throws HttpException, IOException {
+        String generatedDescription = descriptionService.generateDescription(movie.getTitle());
+        movie.setDescription(generatedDescription);
         return movieRepository.save(movie);
     }
 
